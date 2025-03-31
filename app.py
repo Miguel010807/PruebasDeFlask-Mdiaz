71fae3c17f9d5967aa1c74d496fcd69a3f1edbc7
lianleyenda <lianleyende24@gmail.com>
2025-03-31 12:04:00 -0300

    Agregamos otra ruta para la APP

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/Hola/ida')
+def Bienvenido():
+    return 'Bienvenido'
+
+@app.route('/Hola/vuelta')
+def Adios():
+    return 'Adios'
